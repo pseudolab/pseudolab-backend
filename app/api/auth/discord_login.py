77e51de5b8a5ae3d8cb009f128099a7f5d25d4fa
4roring <4roring@naver.com>
2024-02-25 16:22:31 +0900

    discord 로그인 성공까지 확인한 부분 커밋

--- app/api/auth/discord_login.py
+++ app/api/auth/discord_login.py
@@ -4,21 +4,23 @@ from httpx import AsyncClient
 from fastapi import HTTPException
 from fastapi.responses import RedirectResponse
 
+CLINET_ID = "1196826150446571602"
+CLIENT_SECRET = "-74RIQXeoFaDmyPvwtN5UjUpwuCk8hY-"
+REDIRECT_URI = "http://localhost:8000/auth/discord/login/redirect"
 
 @router.get("/discord/login/redirect")
 async def discord_login(code: str) -> RedirectResponse:
     async with AsyncClient() as client:
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
-        body = {
-            "client_id": "1196826150446571602",
-            "client_secret": "uaxTEGKDAxi507plxEGDLt5LwvOfUBDG",
+        data = {
+            "client_id": CLINET_ID,
+            "client_secret": CLIENT_SECRET,
             "grant_type": "authorization_code",
             "code": code,
-            "redirect_uri": "http://localhost:8000/auth/discord/login/redirect",
-            "scope": "identify, email, guilds.join",
+            "redirect_uri": REDIRECT_URI,
+            "scope": "identify",
         }
-
-        response = await client.post("https://discord.com/api/oauth2/token", headers=headers, json=body)
+        response = await client.post("https://discord.com/api/oauth2/token", data=data)
         if not response.is_success:
             raise HTTPException(
                 status_code=500, detail=f"Error in getting token or user data from Discord API: {response.json()}"
@@ -26,7 +28,7 @@ async def discord_login(code: str) -> RedirectResponse:
 
         res_data = response.json()
         access_token = res_data.get("access_token")
-        headers = {"authorization": access_token}
+        headers = {"authorization": f"Bearer {access_token}"}
         response = await client.get("https://discordapp.com/api/users/@me", headers=headers)
         if not response.is_success:
             raise HTTPException(
