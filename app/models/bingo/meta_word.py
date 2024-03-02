39ac692745e3b5936dfeb759a9af627ecea0aa54
ehddnr301 <dy950328@gmail.com>
2024-03-02 13:11:33 +0000

    feat(bingo): Add bingo models
    bingo에 관한 model을 정의하였습니다.

---
+++ app/models/bingo/meta_word.py
@@ -0,0 +1,11 @@
+from app.models.base import Base
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import mapped_column
+
+
+class MetaWord(Base):
+    __tablename__ = "meta_word"
+
+    word_id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    word_type = mapped_column(Integer, nullable=False, default=0)
+    word = mapped_column(String(200), nullable=False)
