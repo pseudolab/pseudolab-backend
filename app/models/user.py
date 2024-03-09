a266f60abdbe26b00747b0711cfd88c53803a43e
4roring <4roring@naver.com>
2024-03-09 19:50:28 +0900

    feat(common): User 테이블의 classmethod session 타입 교체 (여긴 의존성 주입을 받을 수 없음)

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,5 @@
 from models.base import Base
-from core.db import AsyncSessionDepends
+from core.db import AsyncSession
 from sqlalchemy import Integer, String, select
 from sqlalchemy.orm import mapped_column
 from typing import AsyncIterator
@@ -13,13 +13,13 @@ class User(Base):
     email = mapped_column(String(120), unique=True, nullable=False)
 
     @classmethod
-    async def get_user_by_id(cls, session: AsyncSessionDepends, user_id: int):
+    async def get_user_by_id(cls, session: AsyncSession, user_id: int):
         stmt = select(cls).where(cls.id == user_id)
         result = await session.execute(stmt)
         return result.sclar()
 
     @classmethod
-    async def get_user_by_email(cls, session: AsyncSessionDepends, email: str):
+    async def get_user_by_email(cls, session: AsyncSession, email: str):
         stmt = select(cls).where(cls.email == email)
         result = await session.execute(stmt)
         return result.one_or_none()
