6f3566f0ac69be2ffc07291ab9e31f98f2c19132
4roring <4roring@naver.com>
2024-03-31 11:10:15 +0900

    feat(auth): auth router 이름 변경 일부 잘못 적용한 부분 수정

--- app/api/auth/routes.py
+++ app/api/auth/routes.py
@@ -5,26 +5,26 @@ from api.auth.services.social_login import SocialLoginDepends
 auth_router = APIRouter(prefix="/auth")
 
 
-@router.get("/discord/login", description="Discord 로그인 URL 생성")
+@auth_router.get("/discord/login", description="Discord 로그인 URL 생성")
 async def oauth2_login(social_login: SocialLoginDepends) -> LoginResponse:
     return await social_login.discord_login()
 
 
-@router.get("/discord/redirect", description="Discord 로그인 redirect")
+@auth_router.get("/discord/redirect", description="Discord 로그인 redirect")
 async def oauth2_login(login_token: LoginToken, social_login: SocialLoginDepends) -> LoginResponse:
     return await social_login.discord_login_redirect(LoginType.discord, login_token.code)
 
 
-@router.get("/google/login", description="Google 로그인 URL 생성")
+@auth_router.get("/google/login", description="Google 로그인 URL 생성")
 async def oauth2_login(social_login: SocialLoginDepends) -> LoginResponse:
     return await social_login.google_login()
 
 
-@router.get("/google/redirect", description="Google 로그인 redirect")
+@auth_router.get("/google/redirect", description="Google 로그인 redirect")
 async def oauth2_login(login_token: LoginToken, social_login: SocialLoginDepends) -> LoginResponse:
     return await social_login.google_login(login_token.code)
 
 
-@router.get("/sign-up", description="회원가입 API")
+@auth_router.get("/sign-up", description="회원가입 API")
 async def sign_up(social_login: SocialLoginDepends) -> LoginResponse:
     return await social_login.sign_up()
