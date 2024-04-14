888ed2a7b08eb7f7db33e7be70523416a7763829
ehddnr301 <dy950328@gmail.com>
2024-04-14 08:53:04 +0000

    feat(common): Add Cors
    CORS 에러 해결을 위해 코드 추가하였습니다.
    추후 frontend 주소를 origins쪽에 추가하도록 하겠습니다.

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from core.db import db
 from api import api_router
+from starlette.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -15,6 +16,13 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
