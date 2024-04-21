c370cddb16d323c527f7ca607d563549304f9eb0
soohyunme <soohyun.dev@gmail.com>
2024-04-21 10:46:42 +0900

    fix: base model import path

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -6,7 +6,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from app.models import Base
+from models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
