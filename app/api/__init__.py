d25d84f620a32cebea711cc3d5f4cfad4f7d4106
skkim <seungkyukim.dev@gmail.com>
2024-05-27 02:01:01 +0900

    feat: add board crud in init file

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,10 +4,11 @@ from api.auth import auth_router
 from api.bingo.meta_word.routes import meta_word_router
 from api.bingo.bingo_boards.routes import bingo_boards_router
 from api.bingo.bingo_interaction.routes import bingo_interaction_router
+from api.board.crud.routes import boards_router
 
 api_router = APIRouter(prefix="/api")
 
-routers = [auth_router, meta_word_router, bingo_boards_router, bingo_interaction_router]
+routers = [auth_router, meta_word_router, bingo_boards_router, bingo_interaction_router, boards_router]
 
 for router in routers:
     api_router.include_router(router)
