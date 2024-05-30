84d2b149098021c327615833ac6b8d564c345be0
skkim <seungkyukim.dev@gmail.com>
2024-05-30 23:56:20 +0900

    feat: update board model

--- app/models/board/boards.py
+++ app/models/board/boards.py
@@ -27,14 +27,28 @@ class Boards(Base):
 
     @classmethod
     async def create(cls, session: AsyncSession, board_id: int, title: str, content: str, password: int):
-        pass
+        board = await session.get(cls, board_id)
+        if board:
+            raise ValueError(f"{board_id} 게시판 이미 존재.")
+        new_status = Boards(user_id=board_id, title=title, content=content, password=password)
+        session.add(new_status)
+        created_status = await cls.get_board_by_board_id(session, board_id, password)
+        return created_status
 
     @classmethod
-    async def get_board_by_board_id(cls, session: AsyncSession, board_id: int, title: str, content: str, password: int):
-        pass
+    async def get_board_by_board_id(cls, session: AsyncSession, board_id: int, password: int):
+        res = await session.get(cls, board_id, password)
+        if not res:
+            raise ValueError(f"{board_id} 게시판이 존재하지 않습니다.")
 
+        return res
+
+    # 게시판 글 수정시에는 패스워드 다시 입력받지 않는 것으로?
     @classmethod
     async def update_board_by_board_id(
         cls, session: AsyncSession, board_id: int, title: str, content: str, password: int
     ):
-        pass
+        board = await cls.get_board_by_board_id(session, board_id, password)
+        board.board_data.update(title, content)
+
+        return board
