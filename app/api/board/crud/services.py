18a143204f3ac94aecbbbd36b844614b85a619e7
skkim <seungkyukim.dev@gmail.com>
2024-05-30 23:55:35 +0900

    feat: add board crud services

--- app/api/board/crud/services.py
+++ app/api/board/crud/services.py
@@ -12,4 +12,26 @@ class BaseBoard:
 
 class CreateBoard(BaseBoard):
     async def execute(self, board_id: int, title: str, content: str, password: int) -> Boards:
-        pass
+        try:
+            res = await Boards.create(self.async_session, board_id, title, content, password)
+            return BoardResponse(**res.__dict__, ok=True, message="게시판 생성에 성공하였습니다.")
+        except ValueError as e:
+            return BoardResponse(ok=False, message=str(e))
+
+
+class GetBoardByBoardId(BaseBoard):
+    async def execute(self, board_id: int, password: int) -> Boards:
+        try:
+            res = await Boards.get_board_by_board_id(self.async_session, board_id, password)
+            return BoardResponse(**res.__dict__, ok=True, message="게시판 조회에 성공하였습니다.")
+        except ValueError as e:
+            return BoardResponse(ok=False, message=str(e))
+
+
+class UpdateBoardByBoardId(BaseBoard):
+    async def execute(self, board_id: int, title: str, content: str, password: int) -> Boards:
+        try:
+            res = await Boards.update_board_by_userid(self.async_session, board_id, title, content, password)
+            return BoardResponse(**res.__dict__, ok=True, message="게시판 수정에 성공하였습니다.")
+        except ValueError as e:
+            return BoardResponse(ok=False, message=str(e))
