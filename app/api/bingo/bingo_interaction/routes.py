e71bebd0571e81640b4f6519c4006aaf4f292a00
동욱 <dy950328@gmail.com>
2024-06-09 13:57:21 +0900

    Feature/bingo recent interaction (#21)
    * feat(bingo): Add get user by id
    
    유저 id로 유저를 조회하는 기능을 추가했습니다.
    
    * feat(bingo): Add get recent interaction info
    
    user_id를 기반으로 가장 최근 Interaction을 조회하는 기능을 추가했습니다.

--- app/api/bingo/bingo_interaction/routes.py
+++ app/api/bingo/bingo_interaction/routes.py
@@ -1,16 +1,23 @@
 from fastapi import APIRouter, Depends, Path
 
 from .schema import BingoInteractionRequest
-from .services import CreateBingoInteraction
+from .services import CreateBingoInteraction, GetUserLatestInteraction
 
 
 bingo_interaction_router = APIRouter(prefix="/bingo/interactions", tags=["bingo"])
 
 
 @bingo_interaction_router.post("")
-async def create_board(
+async def create_bingo_interaction(
     data: BingoInteractionRequest,
-    bingo_boards: CreateBingoInteraction = Depends(CreateBingoInteraction),
+    bingo_interaction: CreateBingoInteraction = Depends(CreateBingoInteraction),
 ):
-    print(data)
-    return await bingo_boards.execute(**data.model_dump())
+    return await bingo_interaction.execute(**data.model_dump())
+
+
+@bingo_interaction_router.get("/{user_id}")
+async def get_user_latest_interaction(
+    user_id: int = Path(..., title="유저 아이디"),
+    bingo_interaction: GetUserLatestInteraction = Depends(GetUserLatestInteraction),
+):
+    return await bingo_interaction.execute(user_id=user_id)
